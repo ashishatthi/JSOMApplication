49f7433948188a51543e63f88f5dd44ad95d922a
ashish <ashishatthi@hotmail.com>
2015-05-19 12:03:47 +0530

    Added value to var

--- JSOMApplication/First.js
+++ JSOMApplication/First.js
@@ -7,6 +7,6 @@ function GetCustomers()
 
         //*var clientContext = new SP.clientContext.get_current();
     //var oWeb = new clientContext;*///
-    var thisisTest = "";
+    var thisisTest = "Added value";
     
 }
