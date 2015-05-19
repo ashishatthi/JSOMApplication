7fc2f31b176cb4f0320cac870da92486e7ebb0f6
ashish <ashishatthi@hotmail.com>
2015-05-19 12:02:13 +0530

    Added First.js

---
+++ JSOMApplication/First.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="C:\Program Files\SharePoint Client Components\Scripts\MicrosoftAjax.js" />
+/// <reference path="C:\Program Files\SharePoint Client Components\Scripts\SP.debug.js" />
+
+function GetCustomers()
+{
+    var clientContext = SP.ClientContext.get_current
+
+        //*var clientContext = new SP.clientContext.get_current();
+    //var oWeb = new clientContext;*///
+    var thisisTest = "";
+    
+}
