8f412a3b4d3981b02174e937cd83518e374603fc
ankit9319 <kumar.ankit1501@gmail.com>
2015-05-28 14:14:44 +0530

    a

--- JSOMApplication/First.js
+++ JSOMApplication/First.js
@@ -1,11 +1,10 @@
 ﻿/// <reference path="C:\Program Files\SharePoint Client Components\Scripts\MicrosoftAjax.js" />
 /// <reference path="C:\Program Files\SharePoint Client Components\Scripts\SP.debug.js" />
-/// <reference path="C:\Program Files\SharePoint Client Components\Scripts\SP.js" />
-/// <reference path="C:\Program Files\SharePoint Client Components\Scripts\SP.Runtime.js" />
 
 function GetCustomers()
 {
-    var clientContext = new SP.ClientContext("http://susday2084/sites/LeaveTracker/");
+    var clientContext = SP.ClientContext.get_current
+
         //*var clientContext = new SP.clientContext.get_current();
     //var oWeb = new clientContext;*///
     var thisisTest = "Added value";
